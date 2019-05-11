ad2127f2a006c19af93dd43463ab29c63ba2845c
Joe Biellik <joebiellik@gmail.com>
2019-05-11 20:10:28 +0100

    Add 404 page

---
+++ website/pages/en/404.js
@@ -0,0 +1,30 @@
+const React = require("react");
+const CompLibrary = require('../../core/CompLibrary.js');
+const Container = CompLibrary.Container;
+const translate = require('../../server/translate.js').translate;
+
+class ErrorPage extends React.Component {
+  render() {
+    return (
+      <div className="docMainWrapper wrapper">
+        <Container className="mainContainer documentContainer postContainer">
+          <div className="post">
+            <div className="error-message">
+              <h1>404</h1>
+              <h2><translate>Page Not Found</translate></h2>
+              <a href="/"><translate>Return to the home page</translate></a>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+ErrorPage.title = "Page Not Found"
+
+ErrorPage.defaultProps = {
+  language: 'en',
+};
+
+module.exports = ErrorPage;
