70269a5da1753f1f3e315798b4ce4aaf7c75c28d
Joe Biellik <joebiellik@gmail.com>
2019-05-16 17:22:11 +0100

    Add 404 translate descs

--- website/pages/en/404.js
+++ website/pages/en/404.js
@@ -11,8 +11,8 @@ class ErrorPage extends React.Component {
           <div className="post">
             <div className="error-message">
               <h1>404</h1>
-              <h2><translate>Page Not Found</translate></h2>
-              <a href="/"><translate>Return to the home page</translate></a>
+              <h2><translate desc="404 error message">Page Not Found</translate></h2>
+              <a href="/"><translate desc="404 home link">Return to the home page</translate></a>
             </div>
           </div>
         </Container>
